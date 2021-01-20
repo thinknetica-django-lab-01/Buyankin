727468c9ac8445ecd30d51feb0974a725dd187c9
Buyankin <pavelbuyankin@gmail.com>
2021-01-20 17:11:05 +0700

    add links on index.html

--- main/shop/urls.py
+++ main/shop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('about/', about, name='about'),
+    path('contact/', contact, name='contact'),
 ]
--- main/shop/views.py
+++ main/shop/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'shop/index.html')
+
+def about(request):
+    return render(request, 'shop/about.html')
+
+def contact(request):
+    return render(request, 'shop/contact.html')
