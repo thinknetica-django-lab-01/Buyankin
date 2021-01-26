105295bcee71c86648ad643b9c856f71140f1a29
Buyankin <pavelbuyankin@gmail.com>
2021-01-26 22:12:40 +0700

    queries edit

--- main/queries.py
+++ main/queries.py
@@ -3,28 +3,32 @@
 
 python manage.py shell
 
-from shop.models import Shop
-Shop(product='майка', seller='Иван')
-product1 = _
-product1.save()
-product1.id
-
-#второй вариант
-product2 = Shop()
-product2.product = 'кофта'
-product2.seller = 'Маша'
-product2.save()
-
-#третий вариант
-product2 = Shop.objects.create(product='кофта', seller='Маша')
-product2.pk
-
 #проверка запроса к базе данных
 from django.db import connection
 connection.queries
 
+from shop.models import Category
+Category(title='Шорты', slug='shorts')
+category1 = _
+category1.save()
+Category(title='Майки', slug='t-shorts')
+category2 = _
+category2.save()
+category3 = Category.objects.create(title='Тапки', slug='slippers')
+category4 = Category.objects.create(title='Кепки', slug='caps')
+Category.objects.all()
 
 
-from shop.models import Shop
-Shop.objects.all()
-Shop.objects.filter(product='кофта')
+from shop.models import Tags
+tags1 = Tags.objects.create(title='Распродажа', slug='sale')
+tags2 = Tags.objects.create(title='Новинки', slug='new')
+Tags.objects.filter(title='Распродажа')
+Tags.objects.order_by('title')
+
+from shop.models import Seller
+seller1 = Seller.objects.create(name='Иван', description='Обо мне я Иван')
+seller2 = Seller.objects.create(name='Мария', description='Обо мне я Мария')
+
+from shop.models import Product
+product1 = Product.objects.create(title='Майка первая', slug='mayka-pervaya')
+product1 = Product.objects.create(title='Майка вторая', slug='mayka-vtoraya')
