f18e3fe6d191b8d5b272981bc722022ab4c11533
Buyankin <pavelbuyankin@gmail.com>
2021-01-27 15:37:42 +0700

    setup

--- main/shop/urls.py
+++ main/shop/urls.py
@@ -5,4 +5,5 @@ from django.contrib.flatpages import views
 urlpatterns = [
     path('', index, name='index'),
     path('<path:url>', views.flatpage),
+    path('pages/', views.flatpage, {'url': '/about/'}, name='about'),
 ]
